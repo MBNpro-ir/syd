c7c76fe740bdfca1a15bf1493f1e3be93c39d696
MBN <farid.faridi1382@gmail.com>
2025-07-05 04:08:00 +0330

    new folder patch

--- syd.bat
+++ syd.bat
@@ -12,8 +12,10 @@ echo.
 echo Starting SYD (Auto-downloading latest version)...
 echo.
 
+if not exist "SYD" mkdir "SYD"
+
 set SCRIPT_URL="https://github.com/MBNpro-ir/syd/releases/latest/download/syd.ps1"
-set LOCAL_SCRIPT_NAME="syd_latest.ps1"
+set LOCAL_SCRIPT_NAME="SYD\syd_latest.ps1"
 
 echo Downloading the latest version of Simple YouTube Downloader (syd.ps1)...
 powershell -NoProfile -ExecutionPolicy Bypass -Command "try { Invoke-WebRequest -Uri %SCRIPT_URL% -OutFile %LOCAL_SCRIPT_NAME% -UseBasicParsing } catch { Write-Error 'Download failed. Please check your internet connection or the URL.'; exit 1 }"
